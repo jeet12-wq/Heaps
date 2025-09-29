6920c91c45cf84010ff4c6f2b91999402096e435
biswajeet mallik <pikunmallik@gmail.com>
2025-09-29 16:48:40 +0530

    4th comiiitt

---
+++ buildHeap.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+using namespace std;
+
+class Heap
+{
+    int *arr;
+    int capacity;
+    int index = 0;
+
+    public:
+    Heap(int n)
+    {
+        this->capacity = capacity;
+        arr = new int[n];
+        index = 0;
+    }
+
+   
+};
+
+ void heapify(int* arr, int n, int currIndex) {
+	//n -> number of elements in heap
+	int i = currIndex;
+	int leftIndex = 2*i;
+	int rightIndex = 2*i + 1;
+
+	int largestKaIndex = i;
+	//assuming i pr value hi sabse sabse badi value h 
+	//pr ho skta h , sabse badi value left  ya right me hu 
+
+	//check for left
+	if(leftIndex < n && arr[leftIndex] > arr[largestKaIndex]) {
+		//left me badi value mil gyi
+		largestKaIndex = leftIndex;
+	}
+
+	//check for Right
+	if(rightIndex < n && arr[rightIndex] > arr[largestKaIndex]) {
+		//right me badi value mil gyi
+		largestKaIndex = rightIndex;
+	}
+	//jab yaha aaoge, tb tumhare paas, sabse bade value ka index hoga 
+
+	//largestValue still i wali hi h, then no need to do anything
+	//agar largestKaIndex left ya right me se koi, then swap logic
+
+	if(largestKaIndex != i) {
+		//left ya right me se koi h 
+		cout << "replacing "<< arr[i] << " with " << arr[largestKaIndex] << endl;
+		swap(arr[i], arr[largestKaIndex]);
+		i = largestKaIndex;
+		//baaaki recursion sambhal lega 
+		heapify(arr, n, i);
+	}
+}
+
+void buildHeap(int* arr, int n) {
+	for(int i=n/2; i>=1; i--) {
+		heapify(arr, n, i);
+	}
+}
+
+
+   
+
+
+int main()
+{
+    Heap pq(10);
+    int arr[] = {-1, 10,20,30,40,50};
+	int n = 6;
+
+	buildHeap(arr, n);
+	cout<<endl << "Printing heap" << endl;
+	for(int i=1; i<n; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+
+}
